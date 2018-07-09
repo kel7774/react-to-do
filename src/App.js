9a5c1e5bfdda9f3f1d1d644ecd06208478b1d62b
Kelli Landry <kelli.landry@waitrapp.com>
2018-07-09 14:43:28 -0500

    Add the = operator

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
           )}
       </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
-          <input type="text" value={ this.state.newTodoDescription } onChange { (e) => this.handleChange(e) }/>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) }/>
           <input type="submit" />
         </form>
       </div>
