9af2c17d642f10fff89f8a55b0c2ae82e7b27004
Kelli Landry <kelli.landry@waitrapp.com>
2018-07-12 10:34:01 -0500

    Add arrow function in render function. Add assignment of !== to deleteToDo method

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
  }
 
  deleteToDo(index){
-   this.setState( {todos: this.state.todos.filter(todo => todo.isCompleted === false)})
+   this.setState( {todos: this.state.todos.filter(todo => todo !== index)})
  }
 
   render() {
@@ -44,7 +44,7 @@ class App extends Component {
       <div className="App">
       <ul>
        { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description = { todo.description } isCompleted={ todo.isCompleted } toggleComplete= { () => this.toggleComplete(index) } deleteToDo={ this.deleteToDo } />
+            <ToDo key={ index } description = { todo.description } isCompleted={ todo.isCompleted } toggleComplete= { () => this.toggleComplete(index) } deleteToDo={ () => this.deleteToDo(todo) } />
           )}
       </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
